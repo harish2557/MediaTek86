8d6f0ccafcbf1cef4069fb7eb321231c37b3ebf5
Yogachandran <yharishraj2005@gmail.com>
2026-05-19 16:13:46 +0200

    Ajoutez des fichiers projet.

---
+++ MediaTek86/vue/FrmConnexion.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace MediaTek86
+{
+    partial class FrmConnexion
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPwd = new System.Windows.Forms.Label();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.btnConnexion = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPwd
+            // 
+            this.lblPwd.AutoSize = true;
+            this.lblPwd.Location = new System.Drawing.Point(152, 221);
+            this.lblPwd.Name = "lblPwd";
+            this.lblPwd.Size = new System.Drawing.Size(71, 13);
+            this.lblPwd.TabIndex = 0;
+            this.lblPwd.Text = "Mot de passe";
+            this.lblPwd.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Location = new System.Drawing.Point(176, 191);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(36, 13);
+            this.lblLogin.TabIndex = 1;
+            this.lblLogin.Text = "Login ";
+            this.lblLogin.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(305, 221);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(100, 20);
+            this.txtPwd.TabIndex = 2;
+            this.txtPwd.UseSystemPasswordChar = true;
+            this.txtPwd.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(304, 184);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(100, 20);
+            this.txtLogin.TabIndex = 3;
+            this.txtLogin.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // btnConnexion
+            // 
+            this.btnConnexion.Location = new System.Drawing.Point(318, 275);
+            this.btnConnexion.Name = "btnConnexion";
+            this.btnConnexion.Size = new System.Drawing.Size(75, 23);
+            this.btnConnexion.TabIndex = 4;
+            this.btnConnexion.Text = "Connexion";
+            this.btnConnexion.UseVisualStyleBackColor = true;
+            this.btnConnexion.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(318, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Connexion";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // FrmConnexion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 436);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnConnexion);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.lblPwd);
+            this.Name = "FrmConnexion";
+            this.Text = "Connexion";
+            this.Load += new System.EventHandler(this.FrmConnexion_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPwd;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.Button btnConnexion;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
